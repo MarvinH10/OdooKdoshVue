e1e8d9f9d51619f191220520ebe2d341a4033e4f
MarvinH10 <marvinhectorcamposdeza@gmail.com>
2025-01-17 16:30:07 -0500

    Production corrections

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,20 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import { createRouter, createWebHistory } from 'vue-router';
+
+const routes = [
+    {
+        path: '/barcode',
+        name: 'Barcode',
+        component: () => import('@/Pages/Barcode/Barcode.vue'),
+    },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -14,6 +28,7 @@ createInertiaApp({
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
+            .use(router)
             .use(ZiggyVue)
             .mount(el);
     },
